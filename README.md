472beca6ee3e1c089f2ec974ef920a186034f382
infinith4 <infinith4@gmail.com>
2023-10-10 23:31:51 +0900

    mod

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 sudo docker build -t stripeubuntu01:1 ~/projects/github/infinith4/test_docker/test_stripe
 sudo docker run -it stripeubuntu01:1
 
+docker exec -it nodejsdocker01 bash
 
 docker-compose ps
 docker-compose up
--- nodejsdocker01/src/server.js
+++ nodejsdocker01/src/server.js
@@ -6,8 +6,34 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 // App
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
-  res.send('Hello World succeeded request');
+  //res.send('Hello World succeeded request');
+  res.send(`<a href="/result?param1=1&param2=2">GET Method Link</a>
+  <form action="/result" method="POST">
+  <input type="text" name="title[]">
+  <input type="text" name="title[]">
+    <input type="text" name="description">
+    <input type="submit">
+  </form>`);
 });
+
+app.get('/result', (req, res) => {
+  console.log("get /result");
+  const params = req.query;
+  console.log(params);
+  res.send({ result: "success"});
+});
+
+app.post('/result', (req, res) => {
+  console.log("post /result");
+  const params = req.body;
+  console.log(params);
+  console.log(params.title);
+  const titles = params.title;
+  res.send(`success; ${titles.join(", ")}`);
+});
+
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on http://${HOST}:${PORT} http://localhost:${PORT}`);
