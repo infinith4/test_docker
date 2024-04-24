354e7d40b7101b2372d8aff6a56685a5af31f29d
Hiroshi <infinith4@gmail.com>
2024-04-24 13:15:37 +0900

    add

--- test_ubuntu02/src/index.ts
+++ test_ubuntu02/src/index.ts
@@ -1,4 +1,11 @@
 function hello(name: string): string {
+  var aaa=  "aaa";
+  aaa=  "aaa1";
+  aaa=  "aaa2";
+  let bbb =  "bbb";
+  let ccc =  "ccc";
+  let ddd =  "ddd";
+  let eee =  "eee";
   return `Hello, ${name}!`;
 }
 
